eb7d2fbf58c1f6abcd761caa64d7a3f280db49bc
David Nicol <david@sidstan.com>
2018-01-21 23:06:16 +0000

    Unscope subscriber in Gift.rb

--- app/models/shoppe/gift.rb
+++ app/models/shoppe/gift.rb
@@ -4,5 +4,9 @@ module Shoppe
     belongs_to :subscriber, class_name: 'Shoppe::Subscriber'
 
     scope :unclaimed, -> { where(claimed: false) }
+
+    def subscriber
+      Shoppe::Subscriber.unscoped { super }
+    end
   end
 end
