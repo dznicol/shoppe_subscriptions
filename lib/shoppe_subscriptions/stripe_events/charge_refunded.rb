2dee2d52329f1da11ef8775701d467c50cdb34d2
David Nicol <david@sidstan.com>
2018-12-22 14:02:28 +0000

    Change charge_refunded to handle prepays (no invoice)

--- lib/shoppe_subscriptions/stripe_events/charge_refunded.rb
+++ lib/shoppe_subscriptions/stripe_events/charge_refunded.rb
@@ -4,8 +4,10 @@ class ChargeRefunded
   def call(event)
     ActiveRecord::Base.transaction do
       # Handle the incoming invoice succeeded webhook
-
       charge = event.data.object
+
+      refunds = charge.refunds
+      refund_total = refunds.data.sum { |refund| Shoppe::ApiHandler.native_amount(refund.amount) }
 
       if charge.invoice.present?
         invoice = Stripe::Invoice.retrieve(charge.invoice, Shoppe::Stripe.api_key)
@@ -13,14 +15,21 @@ class ChargeRefunded
         subscriber = Shoppe::Subscriber.find_by(stripe_id: invoice.subscription)
 
         if subscriber.present?
-          # Record the transaction for accounting later
-          refunds = charge.refunds
-          total = refunds.data.sum { |refund| Shoppe::ApiHandler.native_amount(refund.amount) }
-
-          subscriber.transactions.create(total: total,
-                                         transaction_type: Shoppe::SubscriberTransaction::TYPES[1])
+          subscriber.transactions.create(total: refund_total, transaction_type: 'refund')
           subscriber.balance -= total
           subscriber.save
+        end
+      elsif charge.customer.present?
+        shoppe_customer = Shoppe::Customer.find_by(stripe_id: charge.customer)
+
+        # Find the first prepay for this subscriber
+        if shoppe_customer.present?
+          prepay_subscriber = shoppe_customer.subscribers.find_by(subscription_plan: nil) || shoppe_customer.subscribers.first
+
+          if prepay_subscriber.present?
+            prepay_subscriber.transactions.create(total: refund_total, transaction_type: 'refund')
+            prepay_subscriber.save
+          end
         end
       end
     end
